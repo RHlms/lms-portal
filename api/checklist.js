bc44a70d5c21c33ce509669ae2940776ba5bc545
RHlms <rh@shortsalestart.com>
2026-05-25 13:39:03 -0400

    Update checklist.js

--- api/checklist.js
+++ api/checklist.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -51,4 +51,4 @@ module.exports = async function handler(req, res) {
     console.error('Handler error:', err);
     return res.status(500).json({ error: err.message });
   }
-};
+}
